55353977570591257cd0a85557cf4987f37bb4b6
John Batte <john.batte@icainformatics.com>
2013-09-30 14:34:54 -0500

    Issues 1 through 3 - done
    * Issue #1 - Added feature file
    * Issue #2 - Added implementation
    * Issue #3 - Added XML code comments

---
+++ src/_specs/Models/RestSharpContext.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+using RestSharp.Injection;
+
+namespace _specs.Models
+{
+	public class RestSharpContext
+	{
+		public IRestSharpFactory Factory { get; set; }
+
+		public IRestClient Client { get; set; }
+
+		public IRestRequest Request { get; set; }
+	}
+}
